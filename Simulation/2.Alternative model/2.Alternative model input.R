05311ce673d5c9c2d80e12e8bed54fecd153ac58
Sunyoung Shin <sunyoung.shin@utdallas.edu>
2021-10-12 10:17:07 -0500

    Simulation updates

--- Simulation/2.Alternative model/2.Alternative model input.R
+++ Simulation/2.Alternative model/2.Alternative model input.R
@@ -1,6 +1,7 @@
 #install.packages("data.table")
 library(data.table)
 #install.packages("markovchain")
+#install.packages('markovchain', repos = 'https://cloud.r-project.org')
 library(markovchain)
 #install.packages("seqinr")
 library(seqinr)
@@ -23,31 +24,29 @@ GenerateMotif<-function(motif,size){
 
 all_indel_info2<- list()
 all_indel_info3<- list()
-GenerateSeq<-function(n_indels,m,motif){
+GenerateSeq2<-function(n_indels,m,motif){
   motifpool<-GenerateMotif(motif,n_indels)
   width<-nrow(motif)
   all_indel_info<- list()
   for (i in seq_len(n_indels)) {
-    insertion_len<-m
-    t<-as.integer(markovchainSequence(n=width*2-3+insertion_len,
+    t<-as.integer(markovchainSequence(n=width*3-3,
                                       mcDNA,t0 = sample(seq(4), 1,prob=prior),include.t0 = TRUE, useRCpp = FALSE))
     
     t1<-t[1:(width-1)]
-    t2<-t[(width+insertion_len):length(t)]
+    t2<-t[(2*width):length(t)]
     t3<-motifpool[i,1:m]
+    t4<-t[(width+m):(2*width-1)] #rest of InDel  
     
-    a1<-n2s(t1-1, levels = c("A", "C", "G", "T"), base4 = TRUE)
-    a2<-n2s(t2-1, levels = c("A", "C", "G", "T"), base4 = TRUE)
     a3<-n2s(t3-1, levels = c("A", "C", "G", "T"), base4 = TRUE)
-    a4<-n2s(t-1, levels = c("A", "C", "G", "T"), base4 = TRUE)
+    a4<-n2s(t4-1, levels = c("A", "C", "G", "T"), base4 = TRUE)
     aref<-n2s(t[(width-1)]-1, levels = c("A", "C", "G", "T"), base4 = TRUE)
     
     ref<-paste(aref, collapse="")
-    alt<-paste((c(aref,a3)), collapse="")
+    alt<-paste((c(aref,a3,a4)), collapse="")
     
     all_indel_info[[i]] <- list(
-      inserted_sequence = as.integer(c(t1,t3,t2)),
-      insertion_len = insertion_len,
+      inserted_sequence = as.integer(c(t1,t3,t4,t2)),
+      insertion_len = width,
       insertion=1,
       ref=ref,
       alt=alt
@@ -62,20 +61,15 @@ GenerateSeq<-function(n_indels,m,motif){
 
 
 #MSC
-k1<-mapply(function(x) GenerateSeq(2000,x,motif_library[1][[1]]),1:10,SIMPLIFY = FALSE)
+k1<-mapply(function(x) GenerateSeq2(2000,x,motif_library[1][[1]]),1:10,SIMPLIFY = FALSE)
 MSC_AL_indel_info<-do.call(c,k1)
 
 #Ddit3::Cebpa
-k1<-mapply(function(x) GenerateSeq(2000,x,motif_library[2][[1]]),1:12,SIMPLIFY = FALSE)
+k1<-mapply(function(x) GenerateSeq2(2000,x,motif_library[2][[1]]),1:12,SIMPLIFY = FALSE)
 Ddit3_AL_indel_info<-do.call(c,k1)
 
 #Hes1
-k1<-mapply(function(x) GenerateSeq(2000,x,motif_library[3][[1]]),1:10,SIMPLIFY = FALSE)
+k1<-mapply(function(x) GenerateSeq2(2000,x,motif_library[3][[1]]),1:10,SIMPLIFY = FALSE)
 Hes1_AL_indel_info<-do.call(c,k1)
 
 save(MSC_AL_indel_info,Ddit3_AL_indel_info,Hes1_AL_indel_info,file="2.AL input.Rdata")
-
-
-
-
-
--- Simulation/2.Alternative model/2.Alternative model test & summary.R
+++ Simulation/2.Alternative model/2.Alternative model test & summary.R
@@ -78,26 +78,11 @@ MSC_list <-mapply(function(x) getP(MSC_AL_pval2,x,2000),1:10,SIMPLIFY = FALSE)
 MSCpa_tbl<-do.call(rbind.data.frame,MSC_list)
 MSCpa_tbl$p<-factor(MSCpa_tbl$p)
 
-fig31<-ggplot(MSCpa_tbl, aes(x=m, y=change.rank, colour=p),size=0.1)+ggtitle("MSC")+ylab("Empirical Power") +ylim(0,1)+
-  geom_line(size=0.1) +scale_x_discrete(name ="Inserted base length", limits=c(2,4,6,8,10))+
-  geom_point()+theme_bw()+theme(title = element_text(size=14),
-                                axis.text=element_text(size=14),axis.title=element_text(size=14),
-                                legend.text = element_text(size=14),legend.title = element_text(size=14),
-                                panel.border = element_blank(), panel.grid.major = element_blank(),
-                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
 #Ddit3::Cebpa
 Ddit3_list <-mapply(function(x) getP(Ddit3_AL_pval2,x,2000),1:12,SIMPLIFY = FALSE)
 Ddit3pa_tbl<-do.call(rbind.data.frame,Ddit3_list)
 Ddit3pa_tbl$p<-factor(Ddit3pa_tbl$p)
-
-fig32<-ggplot(Ddit3pa_tbl, aes(x=m, y=change.rank, colour=p),size=0.1)+ggtitle("Ddit3::Cebpa")+ylab("Empirical Power") +ylim(0,1)+
-  geom_line(size=0.1) +scale_x_discrete(name ="Inserted base length", limits=c(2,4,6,8,10))+
-  geom_point()+theme_bw()+theme(title = element_text(size=14),
-                                axis.text=element_text(size=14),axis.title=element_text(size=14),
-                                legend.text = element_text(size=14),legend.title = element_text(size=14),
-                                panel.border = element_blank(), panel.grid.major = element_blank(),
-                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
 
 #Hes1
@@ -105,13 +90,6 @@ Hes1_list <-mapply(function(x) getP(Hes1_AL_pval2,x,2000),1:10,SIMPLIFY = FALSE)
 Hes1pa_tbl<-do.call(rbind.data.frame,Hes1_list)
 Hes1pa_tbl$p<-factor(Hes1pa_tbl$p)
 
-fig33<-ggplot(Hes1pa_tbl, aes(x=m, y=change.rank, colour=p),size=0.1)+ggtitle("Hes1")+ylab("Empirical Power") +ylim(0,1)+
-  geom_line(size=0.1) +scale_x_discrete(name ="Inserted base length", limits=c(2,4,6,8,10))+
-  geom_point()+theme_bw()+theme(title = element_text(size=14),
-                                axis.text=element_text(size=14),axis.title=element_text(size=14),
-                                legend.text = element_text(size=14),legend.title = element_text(size=14),
-                                panel.border = element_blank(), panel.grid.major = element_blank(),
-                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
 #summary table
 tab31<-MSCpa_tbl[m==10,]
@@ -124,41 +102,214 @@ tab33<-Hes1pa_tbl[m==10,]
 MSC_list_s <-mapply(function(x) getP_s(MSC_AL_pval2,x,2000),1:10,SIMPLIFY = FALSE)
 MSCpa_tbl_s<-do.call(rbind.data.frame,MSC_list_s)
 MSCpa_tbl_s$p<-factor(MSCpa_tbl_s$p)
-fig91<-ggplot(MSCpa_tbl_s, aes(x=m, y=change.score, colour=p),size=0.1)+ggtitle("MSC")+ylab("Empirical Power") +ylim(0,1)+
-  geom_line(size=0.1) +scale_x_discrete(name ="Inserted base length", limits=c(2,4,6,8,10))+
-  geom_point()+theme_bw()+theme(title = element_text(size=14),
-                                axis.text=element_text(size=14),axis.title=element_text(size=14),
-                                legend.text = element_text(size=14),legend.title = element_text(size=14),
-                                panel.border = element_blank(), panel.grid.major = element_blank(),
-                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
 
 #Ddit3::Cebpa
 Ddit3_list_s <-mapply(function(x) getP_s(Ddit3_AL_pval2,x,2000),1:12,SIMPLIFY = FALSE)
 Ddit3pa_tbl_s<-do.call(rbind.data.frame,Ddit3_list_s)
 Ddit3pa_tbl_s$p<-factor(Ddit3pa_tbl_s$p)
-fig92<-ggplot(Ddit3pa_tbl_s, aes(x=m, y=change.score, colour=p),size=0.1)+ggtitle("Ddit3::Cebpa")+ylab("Empirical Power") +ylim(0,1)+
-  geom_line(size=0.1) +scale_x_discrete(name ="Inserted base length", limits=c(2,4,6,8,10))+
-  geom_point()+theme_bw()+theme(title = element_text(size=14),
-                                axis.text=element_text(size=14),axis.title=element_text(size=14),
-                                legend.text = element_text(size=14),legend.title = element_text(size=14),
-                                panel.border = element_blank(), panel.grid.major = element_blank(),
-                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
 
 #Hes1
 Hes1_list_s <-mapply(function(x) getP_s(Hes1_AL_pval2,x,2000),1:10,SIMPLIFY = FALSE)
 Hes1pa_tbl_s<-do.call(rbind.data.frame,Hes1_list_s)
 Hes1pa_tbl_s$p<-factor(Hes1pa_tbl_s$p)
-fig93<-ggplot(Hes1pa_tbl_s, aes(x=m, y=change.score, colour=p),size=0.1)+ggtitle("Hes1")+ylab("Empirical Power") +ylim(0,1)+
-  geom_line(size=0.1) +scale_x_discrete(name ="Inserted base length", limits=c(2,4,6,8,10))+
-  geom_point()+theme_bw()+theme(title = element_text(size=14),
-                                axis.text=element_text(size=14),axis.title=element_text(size=14),
-                                legend.text = element_text(size=14),legend.title = element_text(size=14),
-                                panel.border = element_blank(), panel.grid.major = element_blank(),
-                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
 
 #summary table
 tab71<-MSCpa_tbl_s[m==10,]
 tab72<-Ddit3pa_tbl_s[m==12,]
 tab73<-Hes1pa_tbl_s[m==10,]
+
+MSCpa_tbl_s$Tests<-"Score difference"
+MSCpa_tbl$Tests<-"Binding changer"
+MSCpa_all<-rbind(MSCpa_tbl,MSCpa_tbl_s,use.names=FALSE)
+
+Ddit3pa_tbl_s$Tests<-"Score difference"
+Ddit3pa_tbl$Tests<-"Binding changer"
+Ddit3pa_all<-rbind(Ddit3pa_tbl,Ddit3pa_tbl_s,use.names=FALSE)
+
+Hes1pa_tbl_s$Tests<-"Score difference"
+Hes1pa_tbl$Tests<-"Binding changer"
+Hes1pa_all<-rbind(Hes1pa_tbl,Hes1pa_tbl_s,use.names=FALSE)
+
+
+#p=0.05
+MSC005<-ggplot(MSCpa_all[MSCpa_all$p==0.05,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+   theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                panel.border = element_blank(), panel.grid.major = element_blank(),
+                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+Ddit3005<-ggplot(Ddit3pa_all[Ddit3pa_all$p==0.05,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10,12))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+  theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                panel.border = element_blank(), panel.grid.major = element_blank(),
+                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+Hes1005<-ggplot(Hes1pa_all[Hes1pa_all$p==0.05,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+  theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                panel.border = element_blank(), panel.grid.major = element_blank(),
+                                panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+ggsave(
+  "Hes1005.jpg",
+  plot = Hes1005,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3200,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+ggsave(
+  "MSC005.jpg",
+  plot = MSC005,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3200,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+ggsave(
+  "Ddit3005.jpg",
+  plot = Ddit3005,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3600,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+
+MSC01<-ggplot(MSCpa_all[MSCpa_all$p==0.1,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+  theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                                  axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                                  legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                                  panel.border = element_blank(), panel.grid.major = element_blank(),
+                                                  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+Ddit301<-ggplot(Ddit3pa_all[Ddit3pa_all$p==0.1,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10,12))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+  theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                                  axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                                  legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                                  panel.border = element_blank(), panel.grid.major = element_blank(),
+                                                  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+Hes101<-ggplot(Hes1pa_all[Hes1pa_all$p==0.1,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+  theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                                  axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                                  legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                                  panel.border = element_blank(), panel.grid.major = element_blank(),
+                                                  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+ggsave(
+  "Hes101.jpg",
+  plot = Hes101,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3200,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+ggsave(
+  "MSC01.jpg",
+  plot = MSC01,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3200,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+ggsave(
+  "Ddit301.jpg",
+  plot = Ddit301,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3600,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+MSC001<-ggplot(MSCpa_all[MSCpa_all$p==0.01,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+  theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                                  axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                                  legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                                  panel.border = element_blank(), panel.grid.major = element_blank(),
+                                                  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+Ddit3001<-ggplot(Ddit3pa_all[Ddit3pa_all$p==0.01,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10,12))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+  theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                                  axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                                  legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                                  panel.border = element_blank(), panel.grid.major = element_blank(),
+                                                  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+Hes1001<-ggplot(Hes1pa_all[Hes1pa_all$p==0.01,], aes(x=m, y=change.rank, shape=Tests))+ylab("Empirical Power") +ylim(0,1)+
+  scale_color_manual(values=c("#F8766D","#00BA38"))+geom_line(aes(colour = Tests),size=1)+geom_point(aes(colour = Tests),size=5) +scale_x_discrete(name ="m", limits=c(2,4,6,8,10))+ scale_shape_manual(values = c(17, 16))+theme_bw()+
+  theme(axis.line = element_line(size = 2))+theme(title = element_text(size=30),
+                                                  axis.text=element_text(size=30),axis.title=element_text(size=30),
+                                                  legend.text = element_text(size=30),legend.title = element_text(size=30),
+                                                  panel.border = element_blank(), panel.grid.major = element_blank(),
+                                                  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+ggsave(
+  "Hes1001.jpg",
+  plot = Hes1001,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3200,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+ggsave(
+  "MSC001.jpg",
+  plot = MSC001,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3200,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+ggsave(
+  "Ddit3001.jpg",
+  plot = Ddit3001,
+  device = jpeg,
+  path = NULL,
+  scale = 1,
+  width = 3600,
+  height = 2000,
+  units = "px",
+  dpi = 300)
+
+MSCpa_all[MSCpa_all$p==0.05&m==10,]
+Ddit3pa_all[Ddit3pa_all$p==0.05&m==12,]
+Hes1pa_all[Hes1pa_all$p==0.05&m==10,]
+
 
 save(fig31,fig32,fig33,fig91,fig92,fig93,tab31,tab32,tab33,tab71,tab72,tab73,file="2.AL output.Rdata")
